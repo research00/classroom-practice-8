f0a33e347e12a2e869e3201b07a55bd00cb8b080
Viktor <viktor.schultz00@gmail.com>
2023-05-02 11:51:37 -0700

    1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,22 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        long n = 100000;
+        System.out.println("iteration-> sum 0 ~ " + n + ": " + iteration(n));
+        System.out.println("recursion-> sum 0 ~ " + n + ": " + recursion(n));
+    }
+    public static long iteration(long n) {
+        long sum = 0;
+        for (long i = 0; i <= n; i++) {
+            sum += i;
+        }
+        return sum;
+    }
+
+    public static long recursion(long n) {
+        long sum = 0;
+        if (n >=1) {
+            sum = n + recursion(n - 1);
+        }
+        return sum;
     }
 }
